9741c5de256b939c2a4afaa9f292caf2b5ec670d
kurvashi731-jpg <kurvashi731@gmail.com>
2026-07-09 16:59:05 +0530

    Add interactive Palindrome number

---
+++ palindrome.py
@@ -0,0 +1,8 @@
+def is_palindrome(n):
+    s=str(n)
+    return s==s[::-1]
+num =input("Enter a number:")
+if is_palindrome(num):
+    print(f"{num} is a palindrome.")
+else:
+    print(f"{num} is not  a palindrome.")
